c96c50cdec0c8645466a461ae17b6751bd851c38
Myrmex <dfusi@hotmail.com>
2024-06-21 14:13:23 +0200

    Adding models

---
+++ Cadmus.Epigraphy.Parts/EpiSign.cs
@@ -0,0 +1,73 @@
+﻿using Cadmus.Mat.Bricks;
+using Fusi.Tools.Data;
+using System.Collections.Generic;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Cadmus.Epigraphy.Parts;
+
+/// <summary>
+/// A graphical sign in an epigraphic text. This is a sign of any sort,
+/// either alphabetic or not.
+/// </summary>
+public class EpiSign
+{
+    /// <summary>
+    /// Gets or sets the sign identifier. This is a string uniquely identifying
+    /// the sign, e.g. <c>A</c> or <c>alpha</c> for letter alpha, or a more
+    /// refined classification like <c>alpha_broken_stroke</c>. If you have
+    /// multiple hands, use a naming convention for hands, e.g. <c>A @hand1</c>
+    /// to represent sign <c>A</c> as written by hand 1. The identifier must
+    /// be unique in the scope of its part.
+    /// </summary>
+    /// <remarks>
+    /// Should you want to provide images, you can just use the sign ID plus
+    /// the inscription ID for naming your image file(s). If inscription ID is
+    /// <c>X</c> and sign ID is <c>Y</c>, and we name 2 photographs and 1
+    /// drawing like e.g. <c>X.Y_ph_001.jpg</c>, <c>X.Y_ph_002.jpg</c>,
+    /// <c>X.Y_dr_001.png</c>, we will already have an implicit linking without
+    /// touching the database whenever collecting new images.
+    /// </remarks>
+    public string Id { get; set; } = "";
+
+    /// <summary>
+    /// Gets or sets the features, an optional set of predefined features from
+    /// a thesaurus where you might want to collect recurring graphical features
+    /// you want to use as tags for this sign.This can be a convenient way for
+    /// summarizing the salient points stated in the free text description,
+    /// thus making them searchable.
+    /// </summary>
+    public List<string>? Features { get; set; }
+
+    /// <summary>
+    /// Gets or sets a free text description with formatting (usually Markdown).
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets the sizes keyed by measured element: you can add as many
+    /// sizes as you want for each specific trait you are measuring.For instance,
+    /// a size for vertical ascender, another size for total width, etc.Keys
+    /// are either free or come from a thesaurus when defined.
+    /// </summary>
+    public Dictionary<string, PhysicalSize>? Sizes { get; set; }
+
+    /// <summary>
+    /// Converts to string.
+    /// </summary>
+    /// <returns>
+    /// A <see cref="string" /> that represents this instance.
+    /// </returns>
+    public override string ToString()
+    {
+        StringBuilder sb = new(Id);
+
+        if (Features?.Count > 0)
+            sb.Append(": ").AppendJoin(", ", Features);
+
+        if (Sizes?.Count > 0)
+            sb.Append($" - sizes: {Sizes.Count}");
+
+        return sb.ToString();
+    }
+}
---
+++ Cadmus.Epigraphy.Parts/EpiSupportFr.cs
@@ -0,0 +1,119 @@
+﻿using Cadmus.Mat.Bricks;
+using System.Text;
+
+namespace Cadmus.Epigraphy.Parts;
+
+/// <summary>
+/// A physical fragment of a broken epigraphic material support.
+/// </summary>
+public class EpiSupportFr
+{
+    /// <summary>
+    /// Gets or sets the unique fragment identifier (e.g. <c>A</c>). This is
+    /// unique in the context of its part.
+    /// </summary>
+    public string Id { get; set; } = "";
+
+    /// <summary>
+    /// Gets or sets the shelfmark number for this fragment.
+    /// </summary>
+    public string? Shelfmark { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether this fragment was lost.
+    /// </summary>
+    public bool IsLost { get; set; }
+
+    /// <summary>
+    /// Gets or sets the fragment's size.
+    /// </summary>
+    public PhysicalSize? Size { get; set; }
+
+    /// <summary>
+    /// Gets or sets the row number (1-N) for the relative location of this
+    /// fragment in the inscription, represented as a table where each line
+    /// is a row.
+    /// </summary>
+    public short Row { get; set; }
+
+    /// <summary>
+    /// Gets or sets the column number (1-N) for the relative location of this
+    /// fragment in the inscription, represented as a table where each line
+    /// is a row.
+    /// </summary>
+    public short Column { get; set; }
+
+    /// <summary>
+    /// Gets or sets the rows span for this fragment location.
+    /// </summary>
+    public short RowSpan { get; set; }
+
+    /// <summary>
+    /// Gets or sets the columns span for this fragment location.
+    /// </summary>
+    public short ColumnSpan { get; set; }
+
+    /// <summary>
+    /// Gets or sets the initial portion of the text covered by this fragment,
+    /// used for a more human-friendly reference (e.g. <c>dis manibus</c>...).
+    /// </summary>
+    public string? HeadText { get; set; }
+
+    /// <summary>
+    /// Gets or sets the location of the first character of the text covered
+    /// by this fragment with reference to the inscription. This is typically
+    /// a Cadmus location, e.g. <c>1.3@4</c> meaning line 1, token 3, 4th
+    /// character; but it could be anything, or just omitted, especially when
+    /// you have no text in the database.
+    /// </summary>
+    public string? HeadTextLoc { get; set; }
+
+    /// <summary>
+    /// Gets or sets the final portion of the text covered by this fragment,
+    /// used for a more human-friendly reference (e.g. ...<c>vale</c>).
+    /// </summary>
+    public string? TailText { get; set; }
+
+    /// <summary>
+    /// Gets or sets the location of the last character of the text covered
+    /// by this fragment with reference to the inscription. This is typically
+    /// a Cadmus location, e.g. <c>2.4@2</c> meaning line 2, token 4, 2nd
+    /// character; but it could be anything, or just omitted, especially when
+    /// you have no text in the database.
+    /// </summary>
+    public string? TailTextLoc { get; set; }
+
+    /// <summary>
+    /// Gets or sets a short free text note about this fragment.
+    /// </summary>
+    public string? Note { get; set; }
+
+    /// <summary>
+    /// Converts to string.
+    /// </summary>
+    /// <returns>
+    /// A <see cref="string" /> that represents this instance.
+    /// </returns>
+    public override string ToString()
+    {
+        StringBuilder sb = new(Id);
+
+        if (!string.IsNullOrEmpty(Shelfmark))
+            sb.Append(" [").Append(Shelfmark).Append(']');
+
+        if (IsLost) sb.Append('\u2020');
+
+        if (Size != null) sb.Append(": ").Append(Size);
+
+        if (Row > 0)
+        {
+            sb.Append(" (R").Append(Row);
+            if (RowSpan > 0) sb.Append('×').Append(RowSpan);
+            sb.Append('C').Append(Column);
+            if (ColumnSpan > 0) sb.Append('×').Append(ColumnSpan);
+            sb.Append(')');
+        }
+
+        return sb.ToString();
+    }
+}
